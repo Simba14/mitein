74d015f6695ffe42be018756c80df1590bce52b6
Sean Blundell <seanblundell@gmail.com>
2021-03-13 15:30:54 +0100

    debug redirect

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -21,7 +21,7 @@ const Profile = ({ session }) => {
     t,
   } = useTranslation('profile');
   const userId = get('userId', session);
-
+  console.log({ userId });
   if (!userId) {
     return <Redirect noThrow to={`${language}${ROUTE_LOGIN}`} />;
   }
