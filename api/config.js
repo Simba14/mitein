3552f5ee7a93ac07d4a7d9f952e38931e97f52dd
Sean Blundell <seanblundell@gmail.com>
2021-08-31 13:23:21 +0200

    fix: remove service keys firebase

--- api/config.js
+++ api/config.js
@@ -31,20 +31,6 @@ export default {
       projectId: process.env.FIREBASE_PROJECT_ID,
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     },
-    serviceAccountKeys: {
-      // Has to be remain in snake case
-      type: process.env.FIREBASE_ADMIN_TYPE,
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_AUTH_URI,
-      token_uri: process.env.FIREBASE_TOKEN_URI,
-      auth_provider_x509_cert_url:
-        process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    },
   },
   gcp: {
     email: process.env.GCP_EMAIL,
