5944eab79ea6d372cd2ca5976cfb38ce19a1e66f
Sean Blundell <seanblundell@gmail.com>
2021-08-31 15:13:37 +0200

    fix: i18next config:

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultNS: 'home',
@@ -6,6 +8,7 @@ module.exports = {
     lng: 'en',
     fallbackLng: 'en',
     debug: 'true',
+    localePath: path.resolve('./public/locales'),
     react: {
       transSupportBasicHtmlNodes: true,
       transKeepBasicHtmlNodesFor: ['br', 'strong', 'i'],
