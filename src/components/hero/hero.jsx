938f72847d1053f97a9104a930e1b50d204f334f
Sean Blundell <seanblundell@gmail.com>
2021-10-07 22:20:05 +0200

    fix: hero styling

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import classnames from 'classnames/bind';
 import { useTranslation } from 'next-i18next';
+import { GERMAN } from 'constants/defaultOptions';
 
 import Logo from 'assets/miteinanderEggshell.svg';
 import styles from './hero.module.scss';
+const cx = classnames.bind(styles);
 
 const Hero = () => {
-  const { t } = useTranslation('home');
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation('home');
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.hero}>
-        <h2 className={styles.content}>{t('description')}</h2>
-        <div className={styles.comingSoon}>
-          <Logo className={styles.logo} />
-          <h5 className={styles.text}>{t('comingSoon')}</h5>
+    <div className={cx('wrapper')}>
+      <div className={cx('hero')}>
+        <h2 className={cx('content', { german: language === GERMAN })}>
+          {t('description')}
+        </h2>
+        <div className={cx('comingSoon')}>
+          <Logo className={cx('logo')} />
+          <h5 className={cx('text')}>{t('comingSoon')}</h5>
         </div>
       </div>
     </div>
